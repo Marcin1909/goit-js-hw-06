ecdafeb1225eb90f9c1a68116efcc7a5c61edb19
Marcin <marcinbartnicki2@wp.pl>
2024-06-09 16:05:58 +0200

    Summary (required)

--- js/task-1.js
+++ js/task-1.js
@@ -11,13 +11,13 @@ const customer = {
       return this.discount;
     },
     setDiscount(value) {
-      discount = this.value;
+      this.discount = value;
     },
     getOrders() {
       return this.orders;
     },
     addOrder(cost, order) {
-      balance -= cost - cost * discount;
+      this.balance -= cost - cost * this.discount;
       this.orders.push(order);
     },
     // Change code above this line
